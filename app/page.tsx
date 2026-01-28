e17a089819c61522cdbf0e11d35dac078da3f3ad
jpietraszewski12 <jpietraszewski12@gmail.com>
2026-01-28 18:59:36 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>🍽️ Zamów posiłek szkolny</h1>
+
+      <h2>Menu</h2>
+
+      <ul>
+        <li>🍔 Burger – 10 zł</li>
+        <li>🥪 Kanapka – 6 zł</li>
+        <li>🥗 Sałatka – 8 zł</li>
+      </ul>
+    </main>
+  );
+}
